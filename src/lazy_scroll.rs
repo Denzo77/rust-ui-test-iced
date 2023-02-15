b50940e2629f985be6b1ad3885b446189a65f8a7
Deniz Erbilgin <deniz.erbilgin@gmail.com>
2023-02-15 18:32:17 +0000

    lazy scroll

--- src/lazy_scroll.rs
+++ src/lazy_scroll.rs
@@ -1,5 +1,5 @@
 use iced::{widget::{scrollable, container, text}, Command, Length, Element};
-use iced_native::widget::column;
+use iced_native::{widget::column, Widget};
 
 use crate::Tab;
 
@@ -8,24 +8,32 @@ const DEFAULT_TILE_SIZE: u16 = 128;
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     Scrolled(scrollable::RelativeOffset),
+    Size(u16, u16),
 }
 
 pub struct LazyScroll {
     elements: Vec<String>,
     current_offset: scrollable::RelativeOffset,
+    width: u16,
+    height: u16,
 }
 
 impl LazyScroll {
     pub fn new() -> Self {
         Self {
             elements: (0..100).map(|i| format!("Placeholder-{i}")).collect(),
-            current_offset: scrollable::RelativeOffset::START
+            current_offset: scrollable::RelativeOffset::START,
+            width: 0,
+            height: 0,
         }
     }
 
     pub fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::Scrolled(offset) => { self.current_offset = offset; Command::none()},
+            Message::Scrolled(offset) => {
+                self.current_offset = offset; Command::none()
+            },
+            Message::Size(width, height) => { self.width = width; self.height = height; Command::none() },
         }
     }
 }
@@ -44,7 +52,7 @@ impl Tab for LazyScroll {
     fn content(&self) -> Element<'_, Self::Message> {
         let content = scrollable(
                 column(
-                    self.elements.iter().map(|s| text(s).into()).collect()
+                    self.elements.iter().map(|s| text(s).height(Length::Units(100)).into()).collect()
                 )
                 .width(Length::Fill) //.height(Length::Fill)
             )
